0c1a410b0872e0b7b2ac7cb1818a1744aed0f087
tonymorry <olcrot02@gmail.com>
2025-11-25 23:19:55 +0100

    feat: Update Tailwind CSS content paths
    Expand Tailwind CSS to include new directories for pages, components, and context. This ensures all relevant files are processed for styling.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,10 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./**/*.{js,ts,jsx,tsx}",
+    "./*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./context/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
