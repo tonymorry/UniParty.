9ba26e43a052f3d2792c3bd4eeefa9473b25dfaf
tonymorry <olcrot02@gmail.com>
2025-11-25 21:49:17 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -304,14 +304,12 @@ app.post('/api/stripe/connect', authMiddleware, StripeController.createConnectAc
 app.post('/api/stripe/create-checkout-session', authMiddleware, StripeController.createCheckoutSession);
 
 // --- SERVE STATIC FILES IN PRODUCTION ---
-if (process.env.NODE_ENV === 'production') {
     // Serve any static files
     app.use(express.static(path.join(__dirname, '../dist')));
     // Handle React routing, return all requests to React app
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../dist', 'index.html'));
     });
-}
 
 // Listen
 app.listen(PORT, () => {
